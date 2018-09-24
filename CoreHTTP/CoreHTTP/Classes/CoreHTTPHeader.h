26e4174c6cd5fcc7c66120714a9d8322a0e91d6e
veryitman <veryitman@126.com>
2018-09-24 14:10:28 +0800

    Add lib

---
+++ CoreHTTP/CoreHTTP/Classes/CoreHTTPHeader.h
@@ -0,0 +1,16 @@
+//
+//  CoreHTTPHeader.h
+//  CoreHTTP
+//
+//  Created by mark.zhang on 2018/9/24.
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXTERN const NSString * CoreHTTPLibVersion;
+
+@interface CoreHTTPHeader : NSObject
+
++ (NSString *)coreHTTPLibDesc;
+
+@end
---
+++ CoreHTTP/Example/CoreHTTP/MZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MZAppDelegate.h
+//  CoreHTTP
+//
+//  Created by veryitman@126.com on 09/24/2018.
+//  Copyright (c) 2018 veryitman@126.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
